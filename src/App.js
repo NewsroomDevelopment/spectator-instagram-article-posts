9e98ff1e6598acef83464a6f23b55bf2005d94e5
HongSenDu <hongsendu@gmail.com>
2021-04-13 14:06:55 -0400

    channge format

--- src/App.js
+++ src/App.js
@@ -13,12 +13,12 @@ const Container = styled.div`
   }
 `;
 
-const Title = styled.h1`
-  padding-top: 2.5%;
-  padding-bottom: 2.5%; 
+const Title = styled.p`
+  text-align:center;
+  color:#666;
+  font-size:1em;
   @media (max-width: 800px) {
-    text-align:center;
-    font-size:14px;
+    font-size:12px;
   }
 `;
 
@@ -30,9 +30,49 @@ const Credits = styled.div`
   }
 `;
 
+const Logo = styled.img`  
+  height:5vw;
+  width:5vw;
+  border-radius:50%;
+  z-index:1;
+  @media (max-width: 800px) {
+    height:5vh;
+    width:5vh;
+  }
+`;
+
+const Intro = styled.div`
+  padding-top:1%;
+  display:flex;
+  align-items:center;
+  justify-content:center;
+  :before {
+    z-index:0;
+    content: '';
+    position: absolute;
+    left: 0;
+    border-top: 1px solid #e9e9e9;
+    width: 100%;
+    transform: translateY(-50%);
+  }
+  @media (max-width: 800px) {
+    background-image: linear-gradient(to bottom, white 50%, #f5f5f5 50%);
+  }
+`;
+
+function getWindowDimensions() {
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height
+  };
+}
+
+
 function App() {
 
   const [data, setData] = useState([]);
+  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
 
   useEffect(() => {
     Tabletop.init({
@@ -56,9 +96,12 @@ function App() {
 
   return (
     <div>
+      <Intro>
+        <a target="_blank" href="https://www.columbiaspectator.com/"><Logo src="https://cloudfront-us-east-1.images.arcpublishing.com/spectator/3PTKAMPG6JGXPEYC6L63DZY7MI.png" /></a>
+      </Intro>
       <Container>
-        <Title> Spectator's Instagram Articles List </Title>
-        {data.length > 0 && <Gallery photos={data} onClick={openPhoto} direction={"column"} columns={3} />}
+        <Title> Tap Post for Article </Title>
+        {data.length > 0 && <Gallery photos={data} onClick={openPhoto} direction={"column"} columns={windowDimensions.width > 800 ? 3 : 2} />}
       </Container>
 
       <Credits>
